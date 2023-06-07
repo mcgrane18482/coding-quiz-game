c74a23983d8742e6ff9bfd3e21e4e27f7cb8fa90
Fruit Juice <c.erinmcgrane1@gmail.com>
2023-06-07 15:38:47 -0400

    created an array for questiondata and populated it with one object with question choices and answer

---
+++ assets/questions.js
@@ -0,0 +1,12 @@
+var questionData = [
+    {
+        question:'What does the ID selector do?',
+        choices: ['Apply the style to a specific element', 'Apply the style to all the elements', 'Apply the style to a group of elements', 'Apply the style to elements of the same type'],
+        answer: 'Apply the style to a specific element'
+    },
+    {
+        question: '',
+        choices: [],
+        answer: ''
+    }
+]
